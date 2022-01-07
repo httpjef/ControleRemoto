ddebedf7daf608f0e855dc7a8bb1095858bd0b89
Jeferson <contato.jeferson@gmail.com>
2022-01-06 21:15:38 -0300

    Controle Remoto

---
+++ src/application/aula06.java
@@ -0,0 +1,15 @@
+package application;
+
+public class aula06 {
+
+	public static void main(String[] args) {
+		
+		ControleRemoto c = new ControleRemoto();
+		
+		c.ligar();
+		c.menosVolume();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+
+}
